5a451644cd0d2317970f142442f732b89dd3a665
Mathilde <marco.mathilde@orange.fr>
2025-10-20 12:01:51 +0000

    exo 3

---
+++ ex_3/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "struct.h"
+
+int main()
+{
+    character *perso = malloc(sizeof(*perso));
+
+    perso->name = "Paul";
+    perso->strength = 100;
+    perso->intelligence = 101;
+    perso->wisdom = 50;
+    perso->agility = 80;
+    perso->endurance = 2;
+
+    printf("Le héros c'est %s. Il a %d en force et a %d en intelligence, %d en sagesse, %d en agilité et %d en endurance !\n", perso->name, perso->strength, perso->intelligence, perso->wisdom, perso->agility, perso->endurance);
+    free(perso);
+    exit(0);
+}
